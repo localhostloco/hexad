0bf97e80b686270570c92394eff3b3bb60ad3c9e
Daniel Susumu Ribeiro Yamamoto <daniel.susumu@gmail.com>
2019-06-14 19:31:53 +0200

    Car model added

---
+++ src/main/java/com/hexad/parking/models/Car.java
@@ -0,0 +1,13 @@
+package com.hexad.parking.models;
+
+public class Car {
+  private String color;
+  private String plate;
+
+  public Car(String color, String plate) {
+    this.color = color;
+    this.plate = plate;
+  }
+
+  public Car() {}
+}
