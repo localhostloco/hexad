b6363b662f0ff326fbf7ee032f91786c2cee3fc7
Daniel Susumu Ribeiro Yamamoto <daniel.susumu@gmail.com>
2019-06-15 19:32:04 +0200

    basic ParkingLot methods for its creation

--- src/main/java/com/hexad/parking/models/ParkingLot.java
+++ src/main/java/com/hexad/parking/models/ParkingLot.java
@@ -2,6 +2,14 @@ package com.hexad.parking.models;
 
 public class ParkingLot {
 
-    private ParkingSpot[] availableSpots;
-    private boolean isFull;
+  private ParkingSpot[] availableSpots;
+  private boolean isFull;
+
+  public ParkingLot(int spots) {
+    this.availableSpots = new ParkingSpot[spots];
+  }
+
+  public int getSize() {
+    return availableSpots.length;
+  }
 }
