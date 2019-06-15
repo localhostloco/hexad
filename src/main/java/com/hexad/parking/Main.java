6a9cd4b1ee2458359c8cc450e5c17660e9376b4f
Daniel Susumu Ribeiro Yamamoto <daniel.susumu@gmail.com>
2019-06-15 16:45:49 +0200

    setting both accepted modes

--- src/main/java/com/hexad/parking/Main.java
+++ src/main/java/com/hexad/parking/Main.java
@@ -3,6 +3,13 @@ package com.hexad.parking;
 public class Main {
 
   public static void main(String[] args) {
+    // file input
+    if (args.length > 0) {
 
+    }
+    // interactive mode
+    else {
+
+    }
   }
 }
