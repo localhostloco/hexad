324375b3f03aabfe5f7b12fbda100aca16104500
Daniel Susumu Ribeiro Yamamoto <daniel.susumu@gmail.com>
2019-06-15 17:01:54 +0200

    checking arguments are not null

--- src/test/java/com/hexad/parking/MainTest.java
+++ src/test/java/com/hexad/parking/MainTest.java
@@ -1,4 +1,13 @@
 package com.hexad.parking;
 
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class MainTest {
+
+  @Test
+  public void nullArgumentsThrowAssertionError() {
+    assertThrows(AssertionError.class, () -> Main.main(null));
+  }
 }
