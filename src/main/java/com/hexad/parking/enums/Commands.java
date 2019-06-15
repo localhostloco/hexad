09d2ff46199ddcf91336d356f2d60d63b7c43b10
Daniel Susumu Ribeiro Yamamoto <daniel.susumu@gmail.com>
2019-06-15 17:10:00 +0200

    revamp of commands for ease of use

--- src/main/java/com/hexad/parking/enums/Commands.java
+++ src/main/java/com/hexad/parking/enums/Commands.java
@@ -1,17 +1,11 @@
 package com.hexad.parking.enums;
 
 public enum Commands {
-  CREATE_PARKING("create_parking_lot"),
-  PARK("park"),
-  LEAVE("leave"),
-  STATUS("status"),
-  PLATES_BY_COLOR("registration_numbers_for_cars_with_colour"),
-  SLOTS_BY_COLOR("slot_numbers_for_cars_with_colour"),
-  SLOT_BY_PLATES("slot_number_for_registration_number");
-
-  private String command;
-
-  Commands(String command) {
-    this.command = command;
-  }
+  create_parking_lot,
+  park,
+  leave,
+  status,
+  registration_numbers_for_cars_with_colour,
+  slot_numbers_for_cars_with_colour,
+  slot_number_for_registration_number;
 }
