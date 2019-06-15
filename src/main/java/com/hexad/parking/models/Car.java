be3139e752869c88cfef4fa7ddbcb1a538973f12
Daniel Susumu Ribeiro Yamamoto <daniel.susumu@gmail.com>
2019-06-15 20:52:44 +0200

    changing order of parameters in Car constructor

--- src/main/java/com/hexad/parking/models/Car.java
+++ src/main/java/com/hexad/parking/models/Car.java
@@ -4,7 +4,7 @@ public class Car {
   private String color;
   private String plate;
 
-  public Car(String color, String plate) {
+  public Car(String plate, String color) {
     this.color = color;
     this.plate = plate;
   }
