911be7e224e9a8e4bd789a1b4270e42d3973b74c
Daniel Susumu Ribeiro Yamamoto <daniel.susumu@gmail.com>
2019-06-15 19:51:44 +0200

    enhancing Commands enum for arguments validation per command and lookup method added

--- src/main/java/com/hexad/parking/enums/Commands.java
+++ src/main/java/com/hexad/parking/enums/Commands.java
@@ -1,11 +1,30 @@
 package com.hexad.parking.enums;
 
+import java.util.Arrays;
+
 public enum Commands {
-  create_parking_lot,
-  park,
-  leave,
-  status,
-  registration_numbers_for_cars_with_colour,
-  slot_numbers_for_cars_with_colour,
-  slot_number_for_registration_number;
+  create_parking_lot(1),
+  park(2),
+  leave(1),
+  status(0),
+  registration_numbers_for_cars_with_colour(1),
+  slot_numbers_for_cars_with_colour(1),
+  slot_number_for_registration_number(1);
+
+  private int numberOfArgs;
+
+  Commands(int args) {
+    this.numberOfArgs = args;
+  }
+
+  public int getNumberOfArgs() {
+    return this.numberOfArgs;
+  }
+
+  public static Commands findByCommand(final String command) {
+    return Arrays.stream(values())
+        .filter(value -> value.name().equals(command))
+        .findFirst()
+        .orElse(null);
+  }
 }
