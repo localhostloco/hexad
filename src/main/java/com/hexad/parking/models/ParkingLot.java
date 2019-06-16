348f3a0587128a181b782d9ac6a789471eefa09f
Daniel Susumu Ribeiro Yamamoto <daniel.susumu@gmail.com>
2019-06-16 13:06:37 +0200

    parkCar method added into ParkingLot class

--- src/main/java/com/hexad/parking/models/ParkingLot.java
+++ src/main/java/com/hexad/parking/models/ParkingLot.java
@@ -6,14 +6,22 @@ import java.util.List;
 public class ParkingLot {
 
   private List<ParkingSpot> availableSpots;
+  private Integer nextAvailableSpot;
   private boolean isFull;
 
-  public ParkingLot(int spots) {
+  public ParkingLot(int spots) throws Exception {
+    if (spots > 1) throw new Exception("cannot create a ParkingLot with 0 slots or less");
     this.availableSpots = Arrays.asList(new ParkingSpot[spots]);
+    this.nextAvailableSpot = 0;
   }
 
   public int getSize() {
     return availableSpots.size();
+  }
+
+  public String parkCar(Car car) {
+    String message = "";
+    return message;
   }
 
   public ParkingSpot getParkingSpotBySlot(int slot) {
