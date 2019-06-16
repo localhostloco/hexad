fb7ec6c9cd4a54cf32869041f5513cc34d8f2ab1
Daniel Susumu Ribeiro Yamamoto <daniel.susumu@gmail.com>
2019-06-17 01:08:29 +0200

    ParkingException class created

---
+++ src/main/java/com/hexad/parking/exceptions/ParkingException.java
@@ -0,0 +1,7 @@
+package com.hexad.parking.exceptions;
+
+public class ParkingException extends Exception {
+  public ParkingException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
