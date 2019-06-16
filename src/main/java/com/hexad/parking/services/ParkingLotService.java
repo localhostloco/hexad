af23e16b2e08592fb7a6784d9d421a35fb92ec78
Daniel Susumu Ribeiro Yamamoto <daniel.susumu@gmail.com>
2019-06-16 14:16:21 +0200

    parkCar method added with its implementation

--- src/main/java/com/hexad/parking/services/ParkingLotService.java
+++ src/main/java/com/hexad/parking/services/ParkingLotService.java
@@ -1,8 +1,11 @@
 package com.hexad.parking.services;
 
+import com.hexad.parking.models.Car;
 import com.hexad.parking.models.ParkingLot;
 
 public interface ParkingLotService {
 
   ParkingLot createParkingLot(int spots);
+
+  void parkCar(ParkingLot parkingLot, Car car);
 }
--- src/main/java/com/hexad/parking/services/ParkingLotServiceImpl.java
+++ src/main/java/com/hexad/parking/services/ParkingLotServiceImpl.java
@@ -1,11 +1,22 @@
 package com.hexad.parking.services;
 
+import com.hexad.parking.models.Car;
 import com.hexad.parking.models.ParkingLot;
 
 public class ParkingLotServiceImpl implements ParkingLotService {
   @Override
   public ParkingLot createParkingLot(int spots) {
     System.out.println(String.format("Created a parking lot with %d slots", spots));
-    return new ParkingLot(spots);
+    try {
+      return new ParkingLot(spots);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+
+  @Override
+  public void parkCar(ParkingLot parkingLot, Car car) {
+    System.out.println(parkingLot.parkCar(car));
   }
 }
